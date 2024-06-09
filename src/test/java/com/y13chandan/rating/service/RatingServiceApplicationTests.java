88958a6b67ef34f57043c63d8a9865b2d5fba087
Chandan Kumar singh <chandan@Chandans-MacBook-Pro.local>
2024-06-09 18:53:26 +0530

    initial commit

---
+++ src/test/java/com/y13chandan/rating/service/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y13chandan.rating.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
